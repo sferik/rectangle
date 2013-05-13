cabd67f9d3793a8041e5598915dcabf7c7a9e85f
Erik Michaels-Ober <sferik@gmail.com>
2013-05-13 14:46:07 -0700

    Add failing test for initialize

--- rectangle.rb
+++ rectangle.rb
@@ -6,4 +6,10 @@ require 'rspec'
 
 describe Rectangle do
 
+  it "starts off with a width and height" do
+    rect = Rectangle.new(5, 7)
+    expect(rect.width).to eq 5
+    expect(rect.height).to eq 7
+  end
+
 end
