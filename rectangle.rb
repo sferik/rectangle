eb0b4b01829d303b58275907c501fce880e54d38
Erik Michaels-Ober <sferik@gmail.com>
2013-05-14 09:20:26 -0700

    Revert "Add simplecov and move specs into a different file"
    This reverts commit 9932ab6c510c35f02e24474f28985295a1b763f1.
    I went to fast and want to show this again.

--- rectangle.rb
+++ rectangle.rb
@@ -55,3 +55,77 @@ class Rectangle < Object
   end
 
 end
+
+require 'rspec'
+
+describe Angle do
+
+  it "starts off with a measure in degrees" do
+    angle = Angle.new(90)
+    expect(angle.to_deg).to eq 90
+  end
+
+  it "must be positive" do
+    expect {
+      Angle.new(0)
+    }.to raise_error(ArgumentError)
+  end
+
+  it "is a right angle if it's 90°" do
+    angle = Angle.new(90)
+    expect(angle.right?).to be_true
+  end
+
+  it "is acute if it's less than 90°" do
+    angle = Angle.new(89.9)
+    expect(angle.acute?).to be_true
+  end
+
+  it "is obtuse if it's greater than 90°" do
+    angle = Angle.new(90.1)
+    expect(angle.obtuse?).to be_true
+  end
+
+  it "correctly converts to radians" do
+    angle = Angle.new(90)
+    expect(angle.to_rad).to eq 1.5707963267948966
+  end
+
+  it "is equal to an integer with the same number of degrees" do
+    angle = Angle.new(90)
+    expect(angle == 90).to be_true
+  end
+
+  it "is equal to another angle with the same number of degrees" do
+    angle1 = Angle.new(90)
+    angle2 = Angle.new(90)
+    expect(angle1 == angle2).to be_true
+  end
+
+end
+
+describe Rectangle do
+
+  let(:rect) do
+    Rectangle.new(5, 7)
+  end
+
+  it "starts off with a width and height" do
+    expect(rect.width).to eq 5
+    expect(rect.height).to eq 7
+  end
+
+  it "is a polygon" do
+    expect(rect.sides).to be > 2
+  end
+
+  it "has an area" do
+    expect(rect.area).to eq 35
+  end
+
+  it "has four 90° angles" do
+    expect(rect.angles.length).to eq 4
+    expect(rect.angles.all?{|angle| angle == 90}).to be_true
+  end
+
+end
